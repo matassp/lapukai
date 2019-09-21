219db335a3b0d1a9270d88065593a4a006698b84
matas <matasspeicys@gmail.com>
2019-09-21 16:13:08 +0300

    refactor admin.js functions into leaf class

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,78 +1,21 @@
-var index = 0;
 var localStorage = window.localStorage;
-localStorage.setItem('nextIndex', "0")
 
-function addLeaf() {
-    var e = document.getElementById("specialistSelection");
-    var specialist = e.options[e.selectedIndex].text;
-    var name = document.getElementById("input").value;
-    var leaf = new Leaf(name, specialist);
-    window.localStorage.setItem('leaf', JSON.stringify(leaf))
+function addLeafFromAdminPage() {
+    var specialist = document.getElementById("specialistSelection").value;
+    var clientName = document.getElementById("input").value;
+
+    var leaf = new Leaf(specialist, clientName);
+    leaf.pushToLocalStorage();
 }
 
-class Leaf {
-    constructor(name, specialist) {
-        this.name = name;
-        this.specialist = specialist;
-        this.number = localStorage.getItem('nextIndex');
-        increaseIndex();
-    }
+function loadExampleData() {
+    var leaves = [new Leaf('odontologas', 'Audrius'), new Leaf('ginekologas', 'Urte')]
+    leaves.forEach(function (leaf) {
+        leaf.pushToLocalStorage();
+    })
 }
 
-function increaseIndex() {
-    index++;
-    localStorage.setItem('nextIndex', index.toString())
-}
-
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
-function init() {
-    console.log("aaaa");
-    loadJSON(function (response) {
-        var actual_JSON = JSON.parse(response);
-        console.log(response);
-    });
-
-    var localStorage = window.localStorage;
-
+function clearLocalStorage() {
+    console.log("localStorage cleared");
     localStorage.clear();
-
-    var marius = {
-        specialist: "odontologas",
-        name: "Marius",
-        number: "001"
-    }
-    var antanas = {
-        specialist: "odontologas",
-        name: "Antanas",
-        number: "022"
-    }
-    var ignas = {
-        specialist: "psichologas",
-        name: "Ignas",
-        number: "333"
-    }
-    var jurgis = {
-        specialist: "pediatras",
-        name: "Jurgis",
-        number: "100"
-    }
-
-    localStorage.setItem('marius', JSON.stringify(marius))
-    localStorage.setItem('antanas', JSON.stringify(antanas))
-    localStorage.setItem('ignas', JSON.stringify(ignas))
-    localStorage.setItem('jurgis', JSON.stringify(jurgis))
-    console.log(localStorage.getItem('jurgis'))
 }
---
+++ resources/js/leaf.js
@@ -0,0 +1,40 @@
+// Leaf class that controlls the total amount of leaves by specialist in localStorage
+// Leaf class is able to serialize itself into localStorage
+class Leaf {
+    constructor(specialist, name) {
+        this.name = name;
+        this.isCompleted = false;
+        this.specialist = specialist;
+        this.number = this.getNumber();
+        this.increaseNumber()
+    }
+
+    getNumber() {
+        if (localStorage.getItem(this.specialist + 'Number') != null)
+            return localStorage.getItem(this.specialist + 'Number');
+        this.createLocalStorageFields(this.specialist);
+        return 0;
+    }
+
+    createLocalStorageFields() {
+        console.log('new specialist created')
+        localStorage.setItem(this.specialist, '[]');
+        localStorage.setItem(this.specialist + 'Number', 0);
+    }
+
+    increaseNumber() {
+        var previousNumber = parseInt(this.getNumber());
+        var nextNumber = ++previousNumber;
+        localStorage.setItem(this.specialist + 'Number', nextNumber)
+    }
+
+    complete() {
+        this.isCompleted = true;
+    }
+
+    pushToLocalStorage() {
+        var leafArray = JSON.parse(localStorage.getItem(this.specialist));
+        leafArray.push(this);
+        localStorage.setItem(this.specialist, JSON.stringify(leafArray));
+    }
+}
