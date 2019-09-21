68cd8899096ec916b7bba46e7f7c97db5c60e22f
matas <matasspeicys@gmail.com>
2019-09-21 21:01:03 +0300

    add visual separation for live appointments

--- resources/js/index.js
+++ resources/js/index.js
@@ -30,7 +30,7 @@ function formColumn(specialistName) {
     else {
         html += formColumnHeader(specialistName);
         leafArray.forEach(leaf => {
-            html += formCard(leaf.number, leaf.name, leaf.isCompleted);
+            html += formCard(leaf.number, leaf.name, leaf.isCompleted, specialistName);
         });
     }
 
@@ -41,14 +41,16 @@ function formColumnHeader(specialistName) {
     return '<h1 align="center">' + specialistName + '</h1>';
 }
 
-function formCard(number, clientName, isComplete) {
+function formCard(number, clientName, isComplete, specialistName) {
     var isComplete = isComplete ? 'Taip' : 'Ne';
+    var index = getIndexByNumber(specialistName, number);
+    var timeMessage = (index == 0) ? '<i>Aptarnaujama</i>' : "12:15";
     var template = '<div class="siimple-card"><div class="siimple-card-header" align="center">';
     template += '<div class="siimple-h5 siimple--mb-0">&#127811;</div></div>';
     template += '<div class="siimple-card-body" align="center"><div class="siimple--mt-3 siimple--mb-4" align="center">';
     template = template + '<div class="siimple-h1">' + number.toString() + '</div></div>';
     template = template + '<div class="siimple-h4">' + clientName + '</div>';
-    template += '<div class="siimple-btn siimple-btn--grey siimple-btn--fluid siimple-btn--big siimple--text-bold">12:35</div>';
+    template += '<div class="siimple-btn siimple-btn--grey siimple-btn--fluid siimple-btn--big siimple--text-bold">' + timeMessage + '</div>';
     template += '</div></div>';
     return template;
 };
