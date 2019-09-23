af87e2ba08035ff1a54d43109fe9b4ed11e75f20
matas <matasspeicys@gmail.com>
2019-09-23 12:57:49 +0300

    add time rendering to client.html

--- resources/js/client.js
+++ resources/js/client.js
@@ -9,20 +9,34 @@ function getLeaf() {
     var leaf = getLeafByNumber(parseInt(number));
     var element = document.getElementById("manipulation").firstElementChild;
     if (leaf) {
-        element.innerHTML = formLeaf(leaf);
+        formLeaf(leaf);
+        if (!leaf.isCompleted)
+            updateTime();
     }
     else {
         element.innerHTML = '<h3 align="center">Lapukas nerastas...</h3>';
     }
 }
 
+function updateTime() {
+    var number = document.getElementById("input").value;
+    var element = document.getElementById("time");
+    var leaf = getLeafByNumber(parseInt(number));
+    var index = getFirstNotCompletedLeaf(leaf.specialist).number;
+    element.innerHTML = (index == leaf.number) ? '<i>Aptarnaujama</i>' : getEstTime(leaf) + ' min';
+    console.log('updated');
+    setTimeout(updateTime, 5000);
+}
+
 function formLeaf(leaf) {
-    if (leaf.isCompleted)
-        return '<h3 align="center">Jūs esate aptarnautas</h3>';
     var element = document.getElementById("manipulation").firstElementChild;
+    if (leaf.isCompleted) {
+        element.innerHTML = '<h3 align="center">Jūs esate aptarnautas...</h3>';
+        return;
+    }
     element.id = leaf.specialist;
-    var index = getIndexByNumber(leaf.specialist, leaf.number);
-    var timeMessage = (index == 0) ? '<i>Aptarnaujama</i>' : "12:15";
+    var index = getFirstNotCompletedLeaf(leaf.specialist).number;
+    var timeMessage = (leaf.number == index) ? '<i>Aptarnaujama</i>' : getEstTime(leaf) + ' min';
     var template = '<h1 align="center">';
     template = template + leaf.specialist + '</h1>';
     template += '<div class="siimple-card"><div class="siimple-card-header" align="center">';
@@ -30,7 +44,7 @@ function formLeaf(leaf) {
     template += '<div class="siimple-card-body" align="center"><div class="siimple--mt-3 siimple--mb-4" align="center">';
     template = template + '<div class="siimple-h1">' + leaf.number.toString() + '</div></div>';
     template = template + '<div class="siimple-h4">' + leaf.name + '</div>';
-    template += '<div class="siimple-btn siimple-btn--grey siimple-btn--fluid siimple-btn--big siimple--text-bold">' + timeMessage + '</div>';
+    template += '<div class="siimple-btn siimple-btn--grey siimple-btn--fluid siimple-btn--big siimple--text-bold" id="time">' + timeMessage + '</div>';
     template += '</div></div>';
-    return template;
+    element.innerHTML = template;
 }
